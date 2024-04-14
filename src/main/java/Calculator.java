32378195879972ec95b7d140518fe4bcd359f1e2
tianyazhang <istianyz@163.com>
2024-04-14 14:41:27 +0800

    add calculator test

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,16 @@
+/**
+ * packageName PACKAGE_NAME
+ *
+ * @author 你的名字
+ * @version JDK 8
+ * @className Calculator (此处以class为例)
+ * @date 2024/4/13
+ * @description TODO
+ */
+public class Calculator {
+
+
+    public int compute(int num1, int num2, char symbol) {
+        return 0;
+    }
+}
