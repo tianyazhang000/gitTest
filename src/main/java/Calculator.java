e0de631259c50ed22ea8f3c1664af33c349d1fab
tianyazhang <istianyz@163.com>
2024-04-14 14:48:42 +0800

    add calculator code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -11,6 +11,15 @@ public class Calculator {
 
 
     public int compute(int num1, int num2, char symbol) {
-        return 0;
+
+        switch (symbol){
+            case '+':
+                return num1 + num2;
+            case '-':
+                return num1 - num2;
+            default:
+                throw new IllegalArgumentException();
+        }
+
     }
 }
